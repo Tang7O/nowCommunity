face972ebe09e8aa2daa329762246eb3d4cabf35
Tang7O <1747822506@qq.com>
2021-03-18 17:08:05 +0800

    统一记录日志

---
+++ src/main/java/com/nowcoder/community/aspect/ServiceLogAspect.java
@@ -0,0 +1,37 @@
+package com.nowcoder.community.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Slf4j
+@Component
+@Aspect
+public class ServiceLogAspect {
+
+    @Pointcut("execution(* com.nowcoder.community.service.*.*(..))")
+    public void pointcut() {
+
+    }
+
+    @Before("pointcut()")
+    public void before(JoinPoint joinPoint) {
+        // 用户[1.2.3.4],在[XXX],访问了[com.nowcoder.community.service.xxx()]
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        String ip = request.getRemoteHost();
+        String now = new SimpleDateFormat("yyyy-MM-DD HH:mm:ss").format(new Date());
+        String target = joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName();
+        log.info("用户["+ip+"],在["+now+"],访问了["+target+"].");
+    }
+
+}
