0d1d7934d8b5022ddc24e6771c887e92afb12d6e
Tang7O <1747822506@qq.com>
2021-03-22 21:52:22 +0800

    添加关注功能

--- src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
+++ src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
@@ -5,6 +5,8 @@ public class RedisKeyUtil {
     private static final String SPLIT = ":";
     private static final String PREFIX_ENTITY_LIKE = "like:entity";
     private static final String PREFIX_USER_LIKE = "like:user";
+    private static final String PREFIX_FOLLOWEE = "followee";
+    private static final String PREFIX_FOLLOWER = "follower";
 
     /**
      * 生成某个实体的赞
@@ -23,5 +25,27 @@ public class RedisKeyUtil {
      */
     public static String getUserLikeKey(int userId) {
         return PREFIX_USER_LIKE + SPLIT + userId;
+    }
+
+    /**
+     * 某个用户关注的实体
+     * followee：userId:entityType - > zset(entityId, time)
+     * @param userId     用户ID
+     * @param entityType 关注的实体的类型
+     * @return  返回 Key
+     */
+    public static String getFolloweeKey(int userId, int entityType) {
+        return PREFIX_FOLLOWEE + SPLIT + userId + SPLIT + entityType;
+    }
+
+    /**
+     * 某个实体拥有的粉丝
+     * follower:entityType:entityId -> zset(userId, time)
+     * @param entityType 实体类型
+     * @param entityId   实体ID
+     * @return  返回 Key
+     */
+    public static String getFollowerKey(int entityType, int entityId) {
+        return PREFIX_FOLLOWER + SPLIT + entityType + SPLIT +entityId;
     }
 }
