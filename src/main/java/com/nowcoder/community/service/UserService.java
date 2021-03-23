e127cf3cf36f8d341e3bfe5353a336764f7b281b
Tang7O <1747822506@qq.com>
2021-03-23 22:00:00 +0800

    利用redis优化登录模块

--- src/main/java/com/nowcoder/community/service/UserService.java
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -7,9 +7,11 @@ import com.nowcoder.community.entity.User;
 import com.nowcoder.community.util.CommunityConstant;
 import com.nowcoder.community.util.CommunityUtil;
 import com.nowcoder.community.util.MailClient;
+import com.nowcoder.community.util.RedisKeyUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
@@ -18,6 +20,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class UserService implements CommunityConstant {
@@ -32,7 +35,10 @@ public class UserService implements CommunityConstant {
     private TemplateEngine templateEngine;
 
     @Autowired
-    private LoginTicketMapper loginTicketMapper;
+    private RedisTemplate redisTemplate;
+
+//    @Autowired
+//    private LoginTicketMapper loginTicketMapper;
 
     @Value("${community.path.domain}")
     private String domail;
@@ -41,7 +47,12 @@ public class UserService implements CommunityConstant {
     private String contextPath;
 
     public User findUserById(int id) {
-        return userMapper.selectById(id);
+        //return userMapper.selectById(id);
+        User user = getCache(id);
+        if(user == null) {
+            user = initCache(id);
+        }
+        return user;
     }
 
     public Map<String, Object> register(User user) {
@@ -52,45 +63,45 @@ public class UserService implements CommunityConstant {
             throw new IllegalArgumentException("参数不能为空");
         }
         if (StringUtils.isBlank(user.getUsername())) {
-            map.put("usernameMsg","帐号不能为空");
+            map.put("usernameMsg", "帐号不能为空");
             return map;
         }
         if (StringUtils.isBlank(user.getUsername())) {
-            map.put("passwordMsg","密码不能为空");
+            map.put("passwordMsg", "密码不能为空");
             return map;
         }
         if (StringUtils.isBlank(user.getUsername())) {
-            map.put("emailMsg","邮箱不能为空");
+            map.put("emailMsg", "邮箱不能为空");
             return map;
         }
 
         //验证帐号
         User u = userMapper.selectByName(user.getUsername());
-        if(null != u){
+        if (null != u) {
             map.put("usernameMsg", "该用户名已存在");
             return map;
         }
         // 验证邮箱
         u = userMapper.selectByEmail(user.getEmail());
-        if(null != u){
+        if (null != u) {
             map.put("emailMsg", "该邮箱已被注册");
             return map;
         }
 
         //注册用户
-        user.setSalt(CommunityUtil.generateUUID().substring(0,5));
+        user.setSalt(CommunityUtil.generateUUID().substring(0, 5));
         user.setPassword(CommunityUtil.md5(user.getPassword() + user.getSalt()));
         user.setType(0);
         user.setStatus(0);
         user.setActivationCode(CommunityUtil.generateUUID());
-        user.setHeaderUrl(String.format("http://images.nowcoder.com/head/%dt.png",new Random().nextInt(1005)));
+        user.setHeaderUrl(String.format("http://images.nowcoder.com/head/%dt.png", new Random().nextInt(1005)));
         user.setCreateTime(new Date());
 
         userMapper.insertUser(user);
 
         // 发送激活邮件
         Context context = new Context();
-        context.setVariable("email",user.getEmail());
+        context.setVariable("email", user.getEmail());
         // http://localhost:8080/community/activation/{id}/{activationCode}
         String url = domail + contextPath + "/activation/" + user.getId() + "/" + user.getActivationCode();
         context.setVariable("url", url);
@@ -101,45 +112,46 @@ public class UserService implements CommunityConstant {
         return map;
     }
 
-    public int activation(int userId, String code){
+    public int activation(int userId, String code) {
         User user = userMapper.selectById(userId);
-        if(user.getStatus() == 1) {
+        if (user.getStatus() == 1) {
             return ACTIVATION_REPEAT;
-        } else if(user.getActivationCode().equals(code)) {
-            userMapper.updateStatus(userId,1);
+        } else if (user.getActivationCode().equals(code)) {
+            userMapper.updateStatus(userId, 1);
+            clearCache(userId);
             return ACTIVATION_SUCCESS;
         } else {
             return ACTIVATION_FAILURE;
         }
     }
 
-    public Map<String, Object> login(String username, String password, int expiredSeconds){
+    public Map<String, Object> login(String username, String password, int expiredSeconds) {
         Map<String, Object> map = new HashMap<>();
 
         // 空值处理
-        if(StringUtils.isBlank(username)){
+        if (StringUtils.isBlank(username)) {
             map.put("usernameMsg", "帐号不能为空");
             return map;
         }
-        if(StringUtils.isBlank(password)) {
+        if (StringUtils.isBlank(password)) {
             map.put("passwordMsg", "密码不能为空");
             return map;
         }
 
         // 验证帐号
         User user = userMapper.selectByName(username);
-        if(user == null){
+        if (user == null) {
             map.put("usernameMsg", "该帐号不存在");
             return map;
         }
         // 验证是否激活
-        if(user.getStatus() == 0){
+        if (user.getStatus() == 0) {
             map.put("usernameMsg", "该帐号未激活");
             return map;
         }
         // 验证密码
         password = CommunityUtil.md5(password + user.getSalt());
-        if(!user.getPassword().equals(password)){
+        if (!user.getPassword().equals(password)) {
             map.put("passwordMsg", "密码错误");
             return map;
         }
@@ -150,30 +162,62 @@ public class UserService implements CommunityConstant {
         loginTicket.setTicket(CommunityUtil.generateUUID());
         loginTicket.setStatus(0);
         loginTicket.setExpired(new Date(System.currentTimeMillis() + expiredSeconds * 1000L));
-        loginTicketMapper.insertLoginTicket(loginTicket);
+//        loginTicketMapper.insertLoginTicket(loginTicket);
+
+        String redisKey = RedisKeyUtil.getTicketKey(loginTicket.getTicket());
+        redisTemplate.opsForValue().set(redisKey, loginTicket);
 
         map.put("ticket", loginTicket.getTicket());
         return map;
     }
 
     public void logout(String ticket) {
-        loginTicketMapper.updateStatus(ticket, 1);
+//        loginTicketMapper.updateStatus(ticket, 1);
+        String redisKey = RedisKeyUtil.getTicketKey(ticket);
+        LoginTicket loginTicket = (LoginTicket) redisTemplate.opsForValue().get(redisKey);
+        loginTicket.setStatus(1);
+        redisTemplate.opsForValue().set(redisKey, loginTicket);
     }
 
     public LoginTicket findLoginTicket(String ticket) {
-        return loginTicketMapper.selectByTicket(ticket);
+//        return loginTicketMapper.selectByTicket(ticket);
+        String redisKey = RedisKeyUtil.getTicketKey(ticket);
+        return (LoginTicket) redisTemplate.opsForValue().get(redisKey);
     }
 
-    public int updateHeader(int userId, String headerUrl){
-        return userMapper.updateHeader(userId, headerUrl);
+    public int updateHeader(int userId, String headerUrl) {
+        int rows = userMapper.updateHeader(userId, headerUrl);
+        clearCache(userId);
+        return rows;
     }
 
     public int updatePassword(int userId, String password) {
-        return userMapper.updatePassword(userId, password);
+        int rows = userMapper.updatePassword(userId, password);
+        clearCache(userId);
+        return rows;
     }
 
     public User findUserByName(String name) {
         return userMapper.selectByName(name);
+    }
+
+    private User getCache(int userId) {
+        String redisKey = RedisKeyUtil.getUserKey(userId);
+        return (User) redisTemplate.opsForValue().get(redisKey);
+    }
+
+    private User initCache(int userId) {
+        User user = userMapper.selectById(userId);
+
+        String redisKey = RedisKeyUtil.getUserKey(userId);
+        redisTemplate.opsForValue().set(redisKey, user, 3600, TimeUnit.SECONDS);
+
+        return user;
+    }
+
+    private void clearCache(int userId) {
+        String redisKey = RedisKeyUtil.getUserKey(userId);
+        redisTemplate.delete(redisKey);
     }
 
 }
--- src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
+++ src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
@@ -7,11 +7,14 @@ public class RedisKeyUtil {
     private static final String PREFIX_USER_LIKE = "like:user";
     private static final String PREFIX_FOLLOWEE = "followee";
     private static final String PREFIX_FOLLOWER = "follower";
-
+    private static final String PREFIX_KAPTCHA = "kaptcha";
+    private static final String PREFIX_TICKET = "ticket";
+    private static final String PREFIX_USER = "user";
     /**
      * 生成某个实体的赞
      * like:entity:entityType:entityId -> set(userId)
-      * @return 某个实体的赞
+     *
+     * @return 某个实体的赞
      */
     public static String getEntityLikeKey(int entityType, int entityId) {
         return PREFIX_ENTITY_LIKE + SPLIT + entityType + SPLIT + entityId;
@@ -20,8 +23,9 @@ public class RedisKeyUtil {
     /**
      * 某个用户的赞
      * like:user:userId -> int
+     *
      * @param userId 用户id
-     * @return       用户的赞
+     * @return 用户的赞
      */
     public static String getUserLikeKey(int userId) {
         return PREFIX_USER_LIKE + SPLIT + userId;
@@ -30,9 +34,10 @@ public class RedisKeyUtil {
     /**
      * 某个用户关注的实体
      * followee：userId:entityType - > zset(entityId, time)
+     *
      * @param userId     用户ID
      * @param entityType 关注的实体的类型
-     * @return  返回 Key
+     * @return 返回 Key
      */
     public static String getFolloweeKey(int userId, int entityType) {
         return PREFIX_FOLLOWEE + SPLIT + userId + SPLIT + entityType;
@@ -41,11 +46,41 @@ public class RedisKeyUtil {
     /**
      * 某个实体拥有的粉丝
      * follower:entityType:entityId -> zset(userId, time)
+     *
      * @param entityType 实体类型
      * @param entityId   实体ID
-     * @return  返回 Key
+     * @return 返回 Key
      */
     public static String getFollowerKey(int entityType, int entityId) {
-        return PREFIX_FOLLOWER + SPLIT + entityType + SPLIT +entityId;
+        return PREFIX_FOLLOWER + SPLIT + entityType + SPLIT + entityId;
+    }
+
+    /**
+     * 获取登录验证码的Key
+     *
+     * @param owner 与验证码匹配的”用户“
+     * @return Key
+     */
+    public static String getKaptchaKey(String owner) {
+        return PREFIX_KAPTCHA + SPLIT + owner;
+    }
+
+    /**
+     * 获取登录凭证的key
+     *
+     * @param ticket 凭证
+     * @return key
+     */
+    public static String getTicketKey(String ticket) {
+        return PREFIX_TICKET + SPLIT + ticket;
+    }
+
+    /**
+     * 获取用户的key
+     * @param userId 用户id
+     * @return
+     */
+    public static String getUserKey(int userId) {
+        return PREFIX_USER + SPLIT + userId;
     }
 }
